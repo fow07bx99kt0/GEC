6bb206ed124142d347d49a5e523bb332307c6687
fow07bx99kt0 <felipers0799@gmail.com>
2025-09-03 10:31:22 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+// Config oficial (sem descrições; links SharePoint)
+const CONFIG = {
+  politicas:   { url: "https://corpalloteamentos.sharepoint.com/:f:/s/Corpal/Ej2irJ5cZqVFnucS0ZdM2k4BMUkrfA9-CzDxzu7Y-uPedA?e=ZORaPB" },
+  manuais:     { url: "https://corpalloteamentos.sharepoint.com/:f:/s/Corpal/Etl-sMgvSEJAjILNm91fVWgBcx3to3LOH2LPWuVWqJK03A?e=Dbejb7" },
+  modelos:     { url: "https://corpalloteamentos.sharepoint.com/:f:/s/Corpal/Ei_klHEBwkBEgEfa73GoqJoBzCaytV0EhqlHj93zm1zRkQ?e=q5fGMG" },
+  backoffice:  { url: "https://corpalloteamentos.sharepoint.com/:f:/s/Corpal/EtlFy9qMJBRFoxii5Gef9WIBbHG1VGix4aUHNI9u-1QMBg?e=S7htCq" },
+  engenharia:  { url: "https://corpalloteamentos.sharepoint.com/:f:/s/Corpal/EpHxiROLYTpKhHhUCjJG7BUBOGLrkX151PCb3swmoso3Hw?e=Im7Pd3" },
+  negocios:    { url: "https://corpalloteamentos.sharepoint.com/:f:/s/Corpal/EiYLWBb8wcdAkeSwNcFpeo4BfKc_v3I-KxLQnmeSoJznXw?e=2JetEJ" }
+};
